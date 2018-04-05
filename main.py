fb6f3e5f0dad76ddd42a4d49acc7ff8c056d085e
evanderknaap <eric@vdrknaap.nl>
2018-04-05 15:49:01 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
